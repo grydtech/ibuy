e73c106d96331e5bb8af1dd49c9c3dfc9cd26e91
randilfernando <randil.fernando@outlook.com>
2018-03-29 00:05:13 +0530

    added jax rs application to orderservice

---
+++ order-service/src/main/java/com/grydtech/ibuy/orderservice/requests/OrderCreateRequest.java
@@ -0,0 +1,9 @@
+package com.grydtech.ibuy.orderservice.requests;
+
+public class OrderCreateRequest {
+    private String customerId;
+
+    public String getCustomerId() {
+        return customerId;
+    }
+}
