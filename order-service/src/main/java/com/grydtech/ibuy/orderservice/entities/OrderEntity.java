0c8c05ac3bf53d112445b9e93558784c191dffa7
randilfernando <randil.fernando@outlook.com>
2018-03-29 00:05:13 +0530

    added jax rs application to orderservice

---
+++ order-service/src/main/java/com/grydtech/ibuy/orderservice/entities/OrderEntity.java
@@ -0,0 +1,40 @@
+package com.grydtech.ibuy.orderservice.entities;
+
+import com.grydtech.ibuy.orderservice.events.ItemAddedEvent;
+import com.grydtech.ibuy.orderservice.events.OrderCreatedEvent;
+import com.grydtech.ibuy.orderservice.events.OrderFinalizedEvent;
+
+import java.util.List;
+
+public class OrderEntity {
+    private String orderId;
+    private String customerId;
+    private Double payment;
+    private List<ItemEntity> items;
+
+    public String getOrderId() {
+        return orderId;
+    }
+
+    public String getCustomerId() {
+        return customerId;
+    }
+
+    public Double getPayment() {
+        return payment;
+    }
+
+    public List<ItemEntity> getItems() {
+        return items;
+    }
+
+    public void apply(OrderCreatedEvent orderCreatedEvent) {
+        // add OrderCreatedEvent based logic here
+    }
+    public void apply(ItemAddedEvent itemAddedEvent) {
+        // add ItemAddedEvent based logic here
+    }
+    public void apply(OrderFinalizedEvent orderFinalizedEvent) {
+        // add OrderFinalizedEvent based logic here
+    }
+}
