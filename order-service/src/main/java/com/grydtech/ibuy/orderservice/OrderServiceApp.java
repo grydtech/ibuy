9588b291349c86b21f1c1b1f6033af1a67ba4f52
randilfernando <randil.fernando@outlook.com>
2018-03-30 06:18:56 +0530

    refactored imports added tomcat-embed dependency

---
+++ order-service/src/main/java/com/grydtech/ibuy/orderservice/OrderServiceApp.java
@@ -0,0 +1,8 @@
+package com.grydtech.ibuy.orderservice;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/")
+public class OrderServiceApp extends Application {
+}
