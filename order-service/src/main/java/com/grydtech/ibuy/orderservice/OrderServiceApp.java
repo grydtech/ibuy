5ff7f1c3933f40469aedbe4c3ad37dd8f7ec98a5
randilfernando <randil.fernando@outlook.com>
2018-03-30 08:52:08 +0530

    use jetty server

--- order-service/src/main/java/com/grydtech/ibuy/orderservice/OrderServiceApp.java
+++ order-service/src/main/java/com/grydtech/ibuy/orderservice/OrderServiceApp.java
@@ -1,8 +1,20 @@
 package com.grydtech.ibuy.orderservice;
 
+import com.grydtech.ibuy.orderservice.handlers.CreateOrderHandler;
+
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 @ApplicationPath("/")
 public class OrderServiceApp extends Application {
+    @Override
+    public Set<Class<?>> getClasses() {
+        return new HashSet<>(
+                Arrays.asList(
+                        CreateOrderHandler.class
+                ));
+    }
 }
