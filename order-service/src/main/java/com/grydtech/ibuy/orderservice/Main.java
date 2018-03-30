9870feeb84855161022c6c74a118b8f8c954392e
randilfernando <randil.fernando@outlook.com>
2018-03-30 08:52:08 +0530

    use jetty server

--- order-service/src/main/java/com/grydtech/ibuy/orderservice/Main.java
+++ order-service/src/main/java/com/grydtech/ibuy/orderservice/Main.java
@@ -1,7 +1,30 @@
 package com.grydtech.ibuy.orderservice;
 
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.servlet.ServletContainer;
+
 public class Main {
-    public static void main(String[] args) {
-        
+
+    public static void main(String[] args) throws Exception {
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        context.setContextPath("/");
+
+        Server jettyServer = new Server(8080);
+        jettyServer.setHandler(context);
+
+        ServletContainer jerseyServlet = new ServletContainer(ResourceConfig.forApplicationClass(OrderServiceApp.class));
+        ServletHolder holder = new ServletHolder(jerseyServlet);
+
+        context.addServlet(holder, "/*");
+
+        try {
+            jettyServer.start();
+            jettyServer.join();
+        } finally {
+            jettyServer.destroy();
+        }
     }
 }
